f2138b51ba04d2e79013c48a118e05d92cfabc4a
fjpa121197 <43655840+fjpa121197@users.noreply.github.com>
2020-03-18 17:08:15 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ path = os.getcwd()
 with open('Models/logistic_model.pkl', 'rb') as f:
     logistic = pickle.load(f)
 
-with open('Models/RF_model.pkl', 'rb') as f:
+with open('Models/rf_upload.pkl', 'rb') as f:
     randomforest = pickle.load(f)
 
 with open('Models/svm_clf_model.pkl', 'rb') as f:
     svm_model = pickle.load(f)
 
 
-def get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model):
-    mylist = [Driver_Age, Tax, price, Licence_Length_Years]
+def get_predictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach,
+       exang, oldpeak, slope, ca, thal, req_model):
+    
+    mylist = [age, sex, cp, trestbps, chol, fbs, restecg, thalach,
+       exang, oldpeak, slope, ca, thal]
     mylist = [float(i) for i in mylist]
     vals = [mylist]
 
@@ -43,18 +46,27 @@ def my_form():
 
 @app.route('/', methods=['POST', 'GET'])
 def my_form_post():
-    price = request.form['price']
-    Tax = request.form['Tax']
-    Driver_Age = request.form['Driver_Age']
-    Licence_Length_Years = request.form['Licence_Length_Years']
-    req_model = request.form['req_model']
+    age = request.form['age'] 
+    sex = request.form['sex'] 
+    cp = request.form['cp'] 
+    trestbps = request.form['trestbps'] 
+    chol = request.form['chol'] 
+    fbs = request.form['fbs'] 
+    restecg = request.form['restecg'] 
+    thalach = request.form['thalach'] 
+    exang = request.form['exang'] 
+    oldpeak = request.form['oldpeak'] 
+    slope = request.form['slope'] 
+    ca = request.form['ca'] 
+    thal = request.form['thal'] 
 
-    target = get_predictions(price, Tax, Driver_Age, Licence_Length_Years, req_model)
+    target = get_predictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach,
+       exang, oldpeak, slope, ca, thal, req_model)
 
     if target==1:
-        sale_making = 'Customer is likely to buy the insurance'
+        sale_making = 'You are going to die'
     else:
-        sale_making = 'Customer is unlikely to buy the insurance'
+        sale_making = 'You are safe'
 
     return render_template('home.html', target = target, sale_making = sale_making)
 
